50d0463d7c336f591969e606fb0fa5739cddca84
Javier Granados <hola@javiergranados.dev>
2021-11-30 12:59:55 +0100

    Add HelloWorld screen

--- App.tsx
+++ App.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import HelloWorldScreen from './src/screens/HelloWorldScreen';
 
 const App = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 50,
-          textAlign: 'center',
-        }}
-      >
-        Hello World!
-      </Text>
-    </View>
-  );
+  return <HelloWorldScreen />;
 };
 
 export default App;
---
+++ src/screens/HelloWorldScreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 50,
+          textAlign: 'center',
+        }}
+      >
+        Hello World!
+      </Text>
+    </View>
+  );
+};
+
+export default HelloWorldScreen;
