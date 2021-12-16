0d2562aba3142c2e4d7c7e3c395390c0d1ec2fd3
Javier Granados <hola@javiergranados.dev>
2021-12-16 11:45:30 +0100

    Resolve task 05

--- src/screens/TaskScreens/05.tsx
+++ src/screens/TaskScreens/05.tsx
@@ -15,24 +15,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28435b',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   boxPurple: {
     width: 100,
-    height: 100,
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#5856d6',
   },
   boxOrange: {
     width: 100,
-    height: 100,
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#f0a23b',
   },
   boxBlue: {
     width: 100,
-    height: 100,
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#28c4d9',
